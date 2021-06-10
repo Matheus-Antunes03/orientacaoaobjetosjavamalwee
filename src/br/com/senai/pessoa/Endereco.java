5a02941d751fbb018911d2302639042719235072
Matheus-Antunes03 <matheus_pol@hotmail.com>
2021-06-10 16:39:40 -0300

    Refatoração das classes para herança em Pessoa

--- src/br/com/senai/pessoa/Endereco.java
+++ src/br/com/senai/pessoa/Endereco.java
@@ -1,31 +1,39 @@
 package br.com.senai.pessoa;
 
-public class Endereco {
+public class Endereco extends Cidade {
 
-	//ATRIBUTOS
-	private String cidade;
-	private String estado;
-	private String pais;
+	private String nomeDoBairro;
+	private String nomeDaRua;
+	private String complemento;
+	private String numero;
 	
-	//METODOS
-	public String getCidade() {
-		return cidade;
+	public String getNomeDoBairro() {
+		return nomeDoBairro;
 	}
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
+	public void setNomeDoBairro(String nomeDoBairro) {
+		this.nomeDoBairro = nomeDoBairro;
 	}
-	public String getEstado() {
-		return estado;
+	public String getNomeDaRua() {
+		return nomeDaRua;
 	}
-	public void setEstado(String estado) {
-		this.estado = estado;
+	public void setNomeDaRua(String nomeDaRua) {
+		this.nomeDaRua = nomeDaRua;
 	}
-	public String getPais() {
-		return pais;
+	public String getComplemento() {
+		return complemento;
 	}
-	public void setPais(String pais) {
-		this.pais = pais;
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
 	}
 	
+	
+	
+		
 	
 }
