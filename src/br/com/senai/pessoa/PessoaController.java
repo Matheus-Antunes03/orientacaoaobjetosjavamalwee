2d9a351dc09414bc90417aef5f09c3b47fbbd3b7
Matheus-Antunes03 <matheus_pol@hotmail.com>
2021-06-10 16:36:53 -0300

    Refatoração de controller para adicionar pessoa

--- src/br/com/senai/pessoa/PessoaController.java
+++ src/br/com/senai/pessoa/PessoaController.java
@@ -38,6 +38,35 @@ public class PessoaController {
 		System.out.print("Informe a altura: ");
 		pessoa.setAltura(tec.nextDouble());
 		
+		System.out.print("Informe o país: ");
+		tec.nextLine();
+		pessoa.setNomeDoPais(tec.nextLine());
+		
+		System.out.print("Informe a sigla do país: ");
+		pessoa.setSiglaDoPais(tec.nextLine());
+		
+		System.out.print("Informe o estado: ");
+		pessoa.setNomeDoEstado(tec.nextLine());
+		
+		System.out.print("Informe a sigla do estado: ");
+		pessoa.setUf(tec.nextLine());
+		
+		System.out.print("Informe o nome da cidade: ");
+		pessoa.setNomeDaCidade(tec.nextLine());
+		
+		System.out.print("Informe o nome do bairro: ");
+		pessoa.setNomeDoBairro(tec.nextLine());
+		
+		System.out.print("Informe o nome da rua: ");
+		pessoa.setNomeDaRua(tec.nextLine());
+		
+		System.out.print("Informe um complemento: ");
+		pessoa.setComplemento(tec.nextLine());
+		
+		System.out.print("Informe o número da casa: ");
+		pessoa.setNumero(tec.nextLine());
+		
+		
 		return pessoa;
 	}
 	
@@ -45,14 +74,25 @@ public class PessoaController {
 		
 		System.out.println("--- PESSOAS CADASTRADAS ---");
 		
-		System.out.printf("| %20s | %4s | %5s | %6s |\n", "Nome", "Ano", "Idade", "Altura");
+		System.out.printf("| %12s | %4s | %5s | %6s | %10s | %13s | %15s | %4s | %15s | %15s | %20s | %17s | %6s |\n",
+				"Nome", "Ano", "Idade", "Altura", "País", "Sigla do País", "Estado", "UF", "Cidade", 
+				"Bairro", "Rua", "Complemento", "Número");
 		
 			for(int i = 0; i < pessoas.size(); i++) {
-			System.out.printf("| %20s | %4d | %5d | %4.2f |\n",
+			System.out.printf("| %12s | %4d | %5d | %6.2f | %10s | %13s | %15s | %4s | %15s | %15s | %20s | %17s | %6s |\n",
 					pessoas.get(i).getNome(),
 					pessoas.get(i).getAnoDeNascimento(),
 					pessoas.get(i).getIdade(),
-					pessoas.get(i).getAltura());
+					pessoas.get(i).getAltura(),
+					pessoas.get(i).getNomeDoPais(),
+					pessoas.get(i).getSiglaDoPais(),
+					pessoas.get(i).getNomeDoEstado(),
+					pessoas.get(i).getUf(),
+					pessoas.get(i).getNomeDaCidade(),
+					pessoas.get(i).getNomeDoBairro(),
+					pessoas.get(i).getNomeDaRua(),
+					pessoas.get(i).getComplemento(),
+					pessoas.get(i).getNumero());
 		}
 		return pessoas;
 	}
