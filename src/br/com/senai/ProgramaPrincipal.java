b42b7d065e70fe78dce98315adef6ef972ac7570
Matheus-Antunes03 <matheus_pol@hotmail.com>
2021-06-14 16:41:29 -0300

    Refatoração de classe

--- src/br/com/senai/ProgramaPrincipal.java
+++ src/br/com/senai/ProgramaPrincipal.java
@@ -1,41 +1,56 @@
-package br.com.senai.pessoa;
+package br.com.senai;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import br.com.senai.pessoa.Pessoa;
+import br.com.senai.pessoa.PessoaController;
+import br.com.senai.produto.Produto;
+import br.com.senai.produto.ProdutoController;
 
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) {
 
 		List<Pessoa> pessoas = new ArrayList<>();
-		
+		List<Produto> produtos = new ArrayList<Produto>();
+
 		PessoaController pessoaController = new PessoaController();
-		
+		ProdutoController produtoController = new ProdutoController();
+
 		boolean sair = false;
-		
+
 		do {
 			pessoaController.menu();
-			
+
 			int opcao = pessoaController.leopcao();
-			switch(opcao) {
+			switch (opcao) {
 			case 1:
 				pessoas.add(pessoaController.cadastrarPessoa());
 				break;
-				
+
 			case 2:
 				pessoaController.ListarPessoas(pessoas);
 				break;
-			
+				
+			case 3:
+				produtos.add(produtoController.cadastrarProduto());
+				break;
+				
+			case 4:
+				produtoController.ListarProdutos(produtos);
+				break;
+				
 			case 9:
 				sair = true;
 				break;
-				
+
 			default:
 				System.out.println("Opção Inválida!");
 				break;
 			}
-		}while(!sair);
-		
+		} while (!sair);
+
 		System.out.println("Sistema finalizado!");
 
 	}
